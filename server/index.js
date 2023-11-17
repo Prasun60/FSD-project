31bb404afbd112ddfffeb29a00effae0353d165e
Prasun <prasunmondal60@gmail.com>
2023-11-17 20:27:40 +0530

    server updated

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-mongoose.connect('mongodb://127.0.0.1:27017/Todonotes')
+mongoose.connect('mongodb+srv://abhi:abcd@cluster0.o6xci3c.mongodb.net/')
 .then(() => {
     console.log("Connected to database");
 })
